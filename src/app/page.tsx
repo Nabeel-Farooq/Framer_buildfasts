60887c8ec2e7033844f318802f710d106f347080
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-26 16:40:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-import Image from "next/image";
+
 import Hero from "@components/Hero";
-import { useEffect } from 'react';
-import { useTheme } from 'next-themes';
-import FeaturedIn from "@components/FeaturedIn";
 import ProblemsComponents from "@components/Problems";
 import FeaturesListicle from "@components/FeaturesListicle";
 import About from "@components/About";
@@ -11,34 +8,28 @@ import Pricing from "@components/Pricing";
 import FAQ from "@components/FAQ";
 import CTA from "@components/CTA";
 import Testimonials11 from "@components/TestinomialGrid";
-import HeroCenter from "@components/Hero/HeroCenter";
 import MarqueeFeaturedIn from "@components/featuredIn/marqeeLogos";
-import ImageCardGrid from "@components/ImageCardsGrid";
-import NumberCardGrid from "@components/NumberCardsGrid";
-import IconCardGrid from "@components/IconCardsGrid";
-import FeaturesCarousel from "@components/FeatureCarousel";
-import ImageContainer from "@components/atoms/Cards/Image/ImageContainer";
 
 export default function Home() {
- 
-
   return (
-    <main className=" min-h-screen">
-        <Hero />
-        {/* <HeroCenter/> */}
-        {/* <FeaturedIn /> */}
-        
-       
-        <MarqueeFeaturedIn speed={15}/>
-        <ProblemsComponents />
-        <FeaturesListicle />
-        {/* <FeaturesCarousel/> */}
-        <About />
-        <Pricing />
-        <Testimonials11 />
-        <FAQ/> 
-        <CTA />
-       
+    <main className="min-h-screen">
+      <Hero />
+
+      {/* <HeroCenter /> */}
+      {/* <FeaturedIn /> */}
+
+      <MarqueeFeaturedIn speed={15} />
+
+      <ProblemsComponents />
+      <FeaturesListicle />
+
+      {/* <FeaturesCarousel /> */}
+
+      <About />
+      <Pricing />
+      <Testimonials11 />
+      <FAQ />
+      <CTA />
     </main>
   );
 }
